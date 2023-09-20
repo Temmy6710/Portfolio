a51f2fb65240c844d8f73e19e67408ac5d44f793
Temmy6710 <132060906+Temmy6710@users.noreply.github.com>
2023-09-20 15:53:14 +0100

    Initial commit

---
+++ port/src/loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return(
+        <div className="preloader">
+            <div className="preloader-block">
+                <div className="preloader-icon"><span className="loading-dot loading-dot-1"></span><span className="loading-dot loading-dot-2"></span><span className="loading-dot loading-dot-3"></span></div>
+            </div>
+      </div>
+    );
+}
+
+export default Loading;
